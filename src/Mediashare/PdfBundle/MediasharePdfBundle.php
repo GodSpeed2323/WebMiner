884bd7e9d88a445274b30eaada2007c3b90a1e43
root <root@vps426200.ovh.net>
2017-10-08 05:44:42 +0200

    opti

--- src/Mediashare/PdfBundle/MediasharePdfBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Mediashare\PdfBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MediasharePdfBundle extends Bundle
-{
-}
--- src/Mediashare/ProductBundle/MediashareProductBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Mediashare\ProductBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MediashareProductBundle extends Bundle
-{
-}
